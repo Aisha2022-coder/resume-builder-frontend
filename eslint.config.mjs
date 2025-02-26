785018c46badc236d5b164be0e2ae16d88834661
Aisha2022-coder <aisha.ghosh.cs.2022@mitmeerut.ac.in>
2025-02-27 03:01:44 +0530

    eslint update

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -9,6 +9,15 @@ const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
-const eslintConfig = [...compat.extends("next/core-web-vitals")];
-
+const eslintConfig = [
+  ...compat.extends("next/core-web-vitals"),
+  {
+    settings: {
+      "import/parsers": {
+        "@babel/eslint-parser": [".js", ".mjs"],
+      },
+    },
+  },
+];
 export default eslintConfig;
+
